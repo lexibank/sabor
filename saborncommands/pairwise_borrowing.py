9e3b6e509e1ee7884fd454497ed6348174fc010e
fractaldragonflies <millerje@mac.com>
2022-05-10 13:30:22 -0500

    Moved pairwise borrowing to class with Wordlist parent. Improved get_borrowing function, add train and predict functions. Predict works with parent wordlist or testfile if provided.

--- saborncommands/pairwise_borrowing.py
+++ saborncommands/pairwise_borrowing.py
@@ -9,12 +9,23 @@ May 6, 2022
 
 import tempfile
 from pathlib import Path
+import copy
+import csv
 
 from lexibank_sabor import Dataset as SABOR
 
 import collections
 from lingpy import *
 from pylexibank import progressbar as pb
+
+
+# static helper for is float convertible
+def is_float(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
 
 
 class PairwiseBorrowing(Wordlist):
@@ -35,6 +46,8 @@ class PairwiseBorrowing(Wordlist):
         self.source_id_ = "source_id_"
         self.source_score_ = "source_score_"
         self.source_id = "source_id"
+        self.source_language = "source_language"
+        self.source_segments = "source_tokens"
 
         def get_donor_family():
             # Can optimize function to query wordlist sparsely.
@@ -55,33 +68,65 @@ class PairwiseBorrowing(Wordlist):
             return PairwiseBorrowing(infile=file_path, func=func,
                                      donor=donor, **kw)
 
-    def get_borrowings(self, threshold=0.3, ref="bor_id"):
-        # Calculate the scores if necessary.
-        if (self.source_score_ not in self.columns) or \
-                (self.source_id_ not in self.columns):
-            self.calc_distances()
-        # Add entries for 'ref' when score less than threshold
-        self.add_entries(ref, self.source_id_,
-                         lambda x: x if x != 0 else "", override=True)
-        for (ID, source, score) in self.iter_rows(
-                self.source_id_, self.source_score_):
-            self[ID, ref] = source \
-                if type(score) is float and score < threshold else ""
-        self.current_thr = threshold
+    @staticmethod
+    def subset_wordlist(wordlist, field, values):
+        with tempfile.TemporaryDirectory() as tmp:
+            file_path = Path(tmp).joinpath('temp_wl').as_posix()
+            wordlist.output('tsv', filename=file_path,
+                            ignore='all', prettify=False)
+            file_path += '.tsv'
+            subset = []
+            with open(file_path, newline='') as fl_in:
+                rdr = csv.reader(fl_in, delimiter='\t')
+                hdr = next(rdr)
+                ptr = hdr.index(field.upper())
+                for row in rdr:
+                    if row[ptr] in values:
+                        subset.append(row)
+            with open(file_path, 'w', newline='') as fl_out:
+                wrt = csv.writer(fl_out, delimiter='\t')
+                wrt.writerow(hdr)
+                for row in subset:
+                    wrt.writerow(row)
+            return Wordlist(file_path)
 
-    def calc_distances(self):
+    def get_borrowings(self,  wl=None, threshold=0.3,
+                       ref="bor_id", ref_lang="bor_lang",
+                       ref_segs="bor_segs"):
+
+        wl = wl if wl else self
+        # Calculate the scores if necessary.
+        if (self.source_score_ not in wl.columns) or \
+                (self.source_id_ not in wl.columns):
+            wl = self.calc_distances(wl)
+
+        wl.add_entries(ref, self.source_id_, lambda x: "", override=True)
+        wl.add_entries(ref_lang, self.source_id_, lambda x: "", override=True)
+        wl.add_entries(ref_segs, self.source_id_, lambda x: "", override=True)
+        # Add entries when score < threshold.
+        for (ID, source, score) in wl.iter_rows(
+                self.source_id_, self.source_score_):
+            if is_float(score) and float(score) < threshold:
+                wl[ID, ref] = source
+                wl[ID, ref_lang] = wl[source, 'doculect']
+                wl[ID, ref_segs] = wl[source, self.segments]
+        self.current_thr = threshold
+        return wl  # Need to return wordlist since may not be self.
+
+    def calc_distances(self, wl=None):
+        wl = wl if wl else self
         concepts = {concept: [[], []] for concept in set(
-            [self[idx, self.concept] for idx in self])}
-        for idx in self:
-            if self[idx, "doculect"].startswith(self.donor):
-                concepts[self[idx, self.concept]][0] += [idx]
+            [wl[idx, self.concept] for idx in wl])}
+        for idx in wl:
+            if wl[idx, "doculect"].startswith(self.donor):
+                concepts[wl[idx, self.concept]][0] += [idx]
             # languages from donor family are not target languages.
-            elif self[idx, self.family] != self.donor_family:
-                concepts[self[idx, self.concept]][1] += [idx]
+            elif wl[idx, self.family] != self.donor_family:
+                concepts[wl[idx, self.concept]][1] += [idx]
 
         # iterate over concepts and identify potential borrowings
-        B = {idx: 0 for idx in self}
-        D = {idx: None for idx in self}
+        B = {idx: 0 for idx in wl}
+        D = {idx: None for idx in wl}
         for concept, (donor_indices, target_indices) in pb(concepts.items()):
             # hits is a dictionary with target ID as key and list of possible donor
             # candidate ids as value
@@ -89,21 +134,28 @@ class PairwiseBorrowing(Wordlist):
             for donor_idx in donor_indices:
                 for target_idx in target_indices:
 
-                    score = self.func(self[donor_idx, self.segments],
-                                      self[target_idx, self.segments])
+                    score = self.func(wl[donor_idx, self.segments],
+                                      wl[target_idx, self.segments])
                     hits[target_idx] += [(donor_idx, score)]
             # get the minimum score hit, as we can have only one donor
             for hit, pairs in hits.items():
                 B[hit], D[hit] = sorted(pairs, key=lambda x: x[1])[0]
 
-        self.add_entries(self.source_id_, B,
+        wl.add_entries(self.source_id_, B,
                          lambda x: x if x != 0 else "")
-        self.add_entries(self.source_score_, D,
+        wl.add_entries(self.source_score_, D,
                          lambda x: x if x is not None else "")
 
+        return wl
+
     def train(self, thresholds=None):
+        # Requires gold donor language be specified.
+        # Training wordlist is from parent.
+        if self.gold_donor_language not in self.columns:
+            return
         if thresholds is None:
-            thresholds = [i*0.05 for i in range(20)]
+            thresholds = [i*0.02 for i in range(50)]
+
         best_thr, best_score = 0.0, 0.0
         pred_donor_ref = self.source_id
         for t in thresholds:
@@ -121,6 +173,7 @@ class PairwiseBorrowing(Wordlist):
         # Defensive programming:
         # Check for None and be sure of pred versus gold.
         # Return F1 score overall.
+        # Evaluation wordlist is from parent.
         fn = fp = tn = tp = 0
         for ID, pred_idx, gold_lang in self.iter_rows(pred_ref, gold_ref):
             if not pred_idx:
@@ -134,8 +187,7 @@ class PairwiseBorrowing(Wordlist):
 
         return tp/(tp + (fp + fn)/2)
 
-    def predict(self, words, languages):
-        ...
+    def predict(self, testfile=None, languages=None):
         # Assumes a configured and trained model. Specific function provided
         # at init, optimized for threshold based on training data in wordlist.
         # Predicts borrowing status (?) based on comparison of input entries
@@ -143,6 +195,25 @@ class PairwiseBorrowing(Wordlist):
         # Output is predictions of borrowing from donor language.
         # Indicated by corresponding donor entry pointed to by 'source_id'.
         # For convenience 'source_language' and source_segments' are also shown.
+        # If wl not given, then uses self.
+        # If languages not given, then uses all languages (except donor).
+        # If not trained, uses most recent threshold from get_borrowings.
+
+        if testfile:
+            wl = Wordlist(testfile)
+        else:
+            wl = self
+        if languages:
+            languages = list(set(copy.copy(languages) + [self.donor]))
+            wl = self.subset_wordlist(wl, field='doculect', values=languages)
+        threshold = self.best_thr if self.best_thr else self.current_thr
+        # To predict, get borrowings for wordlist (wl) with best threshold.
+        wl = self.get_borrowings(wl=wl, threshold=threshold,
+                                 ref=self.source_id,
+                                 ref_lang=self.source_language,
+                                 ref_segs=self.source_segments)
+
+        return wl  # Need not be self, so need to return the wordlist.
 
 
 # ============================================
@@ -168,21 +239,24 @@ def edit_distance(seqA, seqB, **kw):
 
 def run_analysis(pairwise, name, threshold, log, report=True):
     """
-    Shortcut for configuring and running an analysis.
+    Shortcut for configuring and running an analysis with specific threshold.
     """
-
     full_name = "{name}-{func}-{thr:.2f}".format(
         name=name, func=pairwise.func.__name__, thr=threshold)
     if report:
         log.info("# ANALYSIS: T={0:.2f}, D={1}, F={2}".format(
             threshold, pairwise.func.__name__, full_name))
-        log.info("## running experiment {}".format(full_name))
 
-    pairwise.get_borrowings(threshold=threshold, ref="source_id")
+    pairwise.get_borrowings(threshold=threshold, ref="source_id",
+                            ref_lang="source_language",
+                            ref_segs="source_tokens")
 
     if report:
         file_path = str(SABOR().dir / "store" / full_name)
-        pairwise.output("tsv", filename=file_path, prettify=False, ignore="all")
+        cols = [col for col in pairwise.columns
+                if col not in ['source_id_', 'source_score_']]
+        pairwise.output("tsv", filename=file_path, prettify=False, ignore="all",
+                        subset=True, cols=cols)
         log.info("## found {0} borrowings".format(
             len([x for x in pairwise if pairwise[x, "source_id"]])))
         log.info("---")
@@ -206,25 +280,42 @@ def get_sabor_wordlist():
 def register(parser):
     parser.add_argument(
         "--infile",
-        default=None,  # "splits/CV10-fold-00-train.tsv",
-        help="wordlist filename containing donor and target language tokens"
+        default=None,  # e.g., "splits/CV10-fold-00-train.tsv"
+        help="wordlist filename containing donor and target language tokens."
     )
     parser.add_argument(
         "--function",
         default="SCA",
         choices=["SCA", "NED"],
-        help="select Needleman edit distance, or sound correspondence alignment"
+        help="select Needleman edit distance, or sound correspondence alignment."
         )
     parser.add_argument(
         "--threshold",
         default=0.3,
         type=float,
-        help="enter <= threshold distance to determine whether likely borrowing"
+        help="enter <= threshold distance to determine whether likely borrowing."
     )
     parser.add_argument(
         "--train",
         action="store_true",
-        help="train pairwise to optimize F1 score"
+        help="train pairwise to optimize F1 score; gold language field required."
+    )
+    parser.add_argument(
+        "--predict",
+        action="store_true",
+        help="predict borrowings based on trained configuration."
+    )
+    parser.add_argument(
+        "--testfile",
+        default=None,  # e.g., "splits/CV10-fold-00-test.tsv"
+        help="wordlist filename containing donor and target language tokens for test."
+    )
+    parser.add_argument(
+        "--language",
+        nargs="*",
+        type=str,
+        default=None,
+        help="languages to use with predict; default is all languages."
     )
 
 
@@ -241,12 +332,27 @@ def run(args):
         wl = get_sabor_wordlist()
         pw = PairwiseBorrowing.from_wordlist(
             wordlist=wl, func=function, donor=donor)
+        args.log.info("Constructed pairwise from SaBor cldf database.")
 
     if args.train:
         pw.train()
         args.log.info("Trained with donor {d}, function {func}".
-                      format(d=pw.donor, func=pw.func))
+                      format(d=pw.donor, func=pw.func.__name__))
         args.log.info("Best: threshold {thr:.2f}, F1 score {f1:.3f}".
                       format(thr=pw.best_thr, f1=pw.best_score))
     else:
         run_analysis(pw, name="pw-spa", threshold=args.threshold, log=args.log)
+
+    if args.predict:
+        languages = "ALL" if args.language is None else args.language
+        args.log.info("Predict with languages {l} and threshold {t}.".
+                      format(l=languages, t=pw.best_thr))
+        wl = pw.predict(testfile=args.testfile, languages=args.language)
+        full_name = "pw-sp-predict-{func}-{thr:.2f}".format(
+            func=pw.func.__name__, thr=pw.best_thr)
+        file_path = str(SABOR().dir / "store" / full_name)
+        cols = [col for col in wl.columns
+                if col not in ['source_id_', 'source_score_']]
+        wl.output("tsv", filename=file_path, prettify=False, ignore="all",
+                  subset=True, cols=cols)
+
